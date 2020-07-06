1b9f4be5ef682a36628ddcc703cf78614716e5e6
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2020-07-06 17:03:12 +0200

    Add documentation
    Generated with `pdoc --html --force --config show_source_code=False potentials_from_particle_insertion`

--- potentials_from_particle_insertion/__init__.py
+++ potentials_from_particle_insertion/__init__.py
@@ -1,5 +1,25 @@
+"""
+Maarten Bransen, 2020
+m.bransen@uu.nl
+"""
+
 __version__ = '0.1'
 
 from .pairpotential_iterator import run_iteration
 from .distribution_functions import rdf_insertion_binned_3d,rdf_insertion_exact_3d,rdf_dist_hist_3d
 
+#define all for doing `from .. import *`
+__all__ = [
+    'run_iteration',
+    'rdf_insertion_binned_3d',
+    'rdf_insertion_exact_3d',
+    'rdf_dist_hist_3d',
+]
+
+#add submodules to pdoc ignore list for generated documentation
+__pdoc__ = {
+    'pairpotential_iterator' : False,
+    'distribution_functions' : False,
+    'geometry' : False,
+    'generate_coordinates' : False
+}
