6c06264124828b3776b82988ff903131c5008e3a
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2020-09-10 09:36:25 +0200

    add plt.show()
    figures don't pop up when running from normal terminal unless you add plt.show()

--- examples/run_example_idealgas_2d.py
+++ examples/run_example_idealgas_2d.py
@@ -5,6 +5,19 @@ import numpy as np
 from potentials_from_particle_insertion import run_iteration,rdf_dist_hist_2d
 from potentials_from_particle_insertion.generate_coordinates import _rand_coord_in_box
 
+params = {
+    'figure.autolayout' : True,
+    'xtick.top' : False,
+    'xtick.bottom' : True,
+    'xtick.minor.visible' : True,
+    'xtick.labelsize':'medium',
+    'ytick.left' : True,
+    'ytick.right' : False,
+    'ytick.minor.visible' : True,
+    'figure.figsize' : (4, 3),
+    'font.size' : 10
+    }
+
 #%% input parameters
 
 #data generation
@@ -37,12 +50,14 @@ binedges,binvals = rdf_dist_hist_2d(
 bincent = (binedges[1:]+binedges[:-1])/2
 
 #plot rdf
-plt.figure()
-plt.plot(bincent,binvals,'-r',label='dist hist')
-plt.xlabel('r')
-plt.ylabel('g(r)')
-plt.xlim(rmin,rmax)
-plt.ylim(0,1.1)
+with plt.rc_context(params):
+    plt.figure()
+    plt.plot(bincent,binvals,'-r',label='dist hist')
+    plt.xlabel('r')
+    plt.ylabel('g(r)')
+    plt.xlim(rmin,rmax)
+    plt.ylim(0,1.1)
+    plt.show()
 
 
 #%% run iterations
@@ -55,26 +70,13 @@ error,potential,rdf,counts = run_iteration(
     dr=dr,
     max_iterations=10,
     convergence_tol=1e-7,
-    periodic_boundary=True,
     n_ins=500,
-    regulate=True
+    regulate=True,
+    periodic_boundary=True,
 )
 
 
 #%% plots
-params = {
-    'figure.autolayout' : True,
-    'xtick.top' : False,
-    'xtick.bottom' : True,
-    'xtick.minor.visible' : True,
-    'xtick.labelsize':'medium',
-    'ytick.left' : True,
-    'ytick.right' : False,
-    'ytick.minor.visible' : True,
-    'figure.figsize' : (4, 3),
-    'font.size' : 10
-    }
-
 from matplotlib.cm import get_cmap
 cmap = get_cmap('jet')
 colors = [cmap(i/(len(error)-1))[:3]+(0.3,) for i in range(len(error))]
@@ -88,6 +90,7 @@ with plt.rc_context(params):
     plt.xlabel('r ($\mathrm{\mu m}$)')
     plt.ylabel('g(r)')
     plt.tight_layout()
+    plt.show()
 
 #plot u(r) for all iterations on top of eachother
 with plt.rc_context(params):
@@ -97,6 +100,7 @@ with plt.rc_context(params):
     plt.xlabel('r ($\mathrm{\mu m}$)')
     plt.ylabel('u(r) $(k_B T)$')
     plt.tight_layout()
+    plt.show()
 
 #plot chi / error as function of iteration number
 with plt.rc_context(params):
@@ -107,6 +111,7 @@ with plt.rc_context(params):
     plt.xlabel('iteration step')
     plt.ylabel('χ²')
     plt.tight_layout()
+    plt.show()
 
 #plot distance histogram and last iteration together
 with plt.rc_context(params):
@@ -119,6 +124,7 @@ with plt.rc_context(params):
     plt.xlim(rmin,rmax)
     plt.legend(frameon=False)
     plt.tight_layout()
+    plt.show()
 
 #plot u(r) of last iteration
 with plt.rc_context(params):
@@ -129,3 +135,4 @@ with plt.rc_context(params):
     plt.ylabel('$u(r)$ $(k_B T)$')
     plt.xlim([rmin,rmax])
     plt.tight_layout()
+    plt.show()
--- examples/run_example_idealgas_3d.py
+++ examples/run_example_idealgas_3d.py
@@ -5,6 +5,19 @@ import numpy as np
 from potentials_from_particle_insertion import run_iteration,rdf_dist_hist_3d
 from potentials_from_particle_insertion.generate_coordinates import _rand_coord_in_box #only for generating fake data
 
+params = {
+    'figure.autolayout' : True,
+    'xtick.top' : False,
+    'xtick.bottom' : True,
+    'xtick.minor.visible' : True,
+    'xtick.labelsize':'medium',
+    'ytick.left' : True,
+    'ytick.right' : False,
+    'ytick.minor.visible' : True,
+    'figure.figsize' : (4, 3),
+    'font.size' : 10
+    }
+
 #%% input parameters
 
 #data generation
@@ -37,11 +50,14 @@ binedges,binvals = rdf_dist_hist_3d(
 bincent = (binedges[1:]+binedges[:-1])/2
 
 #plot rdf
-plt.figure()
-plt.plot(bincent,binvals,'-r',label='dist hist')
-plt.xlabel('r ($\mathrm{\mu m}$)')
-plt.ylabel('g(r)')
-plt.xlim(rmin,rmax)
+with plt.rc_context(params):
+    plt.figure()
+    plt.plot(bincent,binvals,'-r',label='dist hist')
+    plt.xlabel('r ($\mathrm{\mu m}$)')
+    plt.ylabel('g(r)')
+    plt.xlim(rmin,rmax)
+    plt.ylim(0,1.1)
+    plt.show()
 
 
 #%% run iterations
@@ -55,24 +71,12 @@ error,potential,rdf,counts = run_iteration(
     max_iterations=10,
     convergence_tol=1e-6,
     n_ins=500,
-    regulate=True
+    regulate=True,
+    periodic_boundary=False,
 )
 
 
 #%% plots
-params = {
-    'figure.autolayout' : True,
-    'xtick.top' : False,
-    'xtick.bottom' : True,
-    'xtick.minor.visible' : True,
-    'xtick.labelsize':'medium',
-    'ytick.left' : True,
-    'ytick.right' : False,
-    'ytick.minor.visible' : True,
-    'figure.figsize' : (4, 3),
-    'font.size' : 10
-    }
-
 from matplotlib.cm import get_cmap
 cmap = get_cmap('jet')
 colors = [cmap(i/(len(error)-1))[:3]+(0.3,) for i in range(len(error))]
@@ -86,6 +90,7 @@ with plt.rc_context(params):
     plt.xlabel('r ($\mathrm{\mu m}$)')
     plt.ylabel('g(r)')
     plt.tight_layout()
+    plt.show()
 
 #plot u(r) for all iterations on top of eachother
 with plt.rc_context(params):
@@ -95,6 +100,7 @@ with plt.rc_context(params):
     plt.xlabel('r ($\mathrm{\mu m}$)')
     plt.ylabel('u(r) $(k_B T)$')
     plt.tight_layout()
+    plt.show()
 
 #plot chi / error as function of iteration number
 with plt.rc_context(params):
@@ -105,6 +111,7 @@ with plt.rc_context(params):
     plt.xlabel('iteration step')
     plt.ylabel('χ²')
     plt.tight_layout()
+    plt.show()
 
 #plot distance histogram and last iteration together
 with plt.rc_context(params):
@@ -117,6 +124,7 @@ with plt.rc_context(params):
     plt.xlim(rmin,rmax)
     plt.legend(frameon=False)
     plt.tight_layout()
+    plt.show()
 
 #plot u(r) of last iteration
 with plt.rc_context(params):
@@ -127,3 +135,4 @@ with plt.rc_context(params):
     plt.ylabel('$u(r)$ $(k_B T)$')
     plt.xlim([rmin,rmax])
     plt.tight_layout()
+    plt.show()
