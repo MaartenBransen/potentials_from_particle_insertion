6d1e4121f282fa822cd7e739f21a36505627741b
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2022-05-10 14:15:02 +0200

    fix examples

--- examples/run_example_fitfunction_3d.py
+++ examples/run_example_fitfunction_3d.py
@@ -4,19 +4,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 from potentials_from_particle_insertion import run_iterator_fitfunction,rdf_dist_hist_3d
 from potentials_from_particle_insertion.generate_coordinates import _rand_coord_in_box #only for generating fake data
-
-params = {
-    'figure.autolayout' : True,
-    'xtick.top' : False,
-    'xtick.bottom' : True,
-    'xtick.minor.visible' : True,
-    'xtick.labelsize':'medium',
-    'ytick.left' : True,
-    'ytick.right' : False,
-    'ytick.minor.visible' : True,
-    'figure.figsize' : (4, 3),
-    'font.size' : 10
-    }
 plt.close('all')
 
 #%% input parameters
@@ -51,26 +38,24 @@ binedges,binvals = rdf_dist_hist_3d(
     rmax=rmax,
     dr=dr,
     boundary=boundary,
-    periodic_boundary=False
+    handle_edge='cuboid',
 )
 
 bincent = (binedges[1:]+binedges[:-1])/2
 
 #plot rdf
-with plt.rc_context(params):
-    plt.figure()
-    plt.plot(bincent,binvals,'-r',label='dist hist')
-    plt.xlabel('r ($\mathrm{\mu m}$)')
-    plt.ylabel('g(r)')
-    plt.xlim(rmin,rmax)
-    plt.ylim(0,1.1)
+plt.figure()
+plt.plot(bincent,binvals,'-r',label='dist hist')
+plt.xlabel('r ($\mathrm{\mu m}$)')
+plt.ylabel('g(r)')
+plt.xlim(rmin,rmax)
+plt.ylim(0,1.1)
 plt.show()
 
 #%% run iterations
 error,potential,fitparams,rdf,counts = run_iterator_fitfunction(
     coords,
     binvals,
-    boundary,
     fitfun,
     rmin=rmin,
     rmax=rmax,
@@ -80,8 +65,8 @@ error,potential,fitparams,rdf,counts = run_iterator_fitfunction(
     max_iterations=10,
     convergence_tol=1e-6,
     n_ins=1000,
-    #regulate=True,
-    periodic_boundary=False,
+    boundary=boundary,
+    handle_edge='cuboid',
 )
 
 
@@ -91,56 +76,51 @@ cmap = get_cmap('jet')
 colors = [cmap(i/(len(error)-1))[:3]+(0.3,) for i in range(len(error))]
 
 #plot all iterations on top of eachother
-with plt.rc_context(params):
-    fig = plt.figure()
-    plt.plot(bincent,binvals,label='distance histogram',color='k')
-    [plt.plot(bincent,rdf[i],label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
-    plt.xlim(rmin,rmax)
-    plt.xlabel('r ($\mathrm{\mu m}$)')
-    plt.ylabel('g(r)')
-    plt.tight_layout()
-    
+fig = plt.figure()
+plt.plot(bincent,binvals,label='distance histogram',color='k')
+[plt.plot(bincent,rdf[i],label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
+plt.xlim(rmin,rmax)
+plt.xlabel('r ($\mathrm{\mu m}$)')
+plt.ylabel('g(r)')
+plt.tight_layout()
+
 #plot u(r) for all iterations on top of eachother
 plotx = np.linspace(rmin,rmax,200)
-with plt.rc_context(params):
-    fig = plt.figure()
-    [plt.plot(plotx,potential[i](plotx),label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
-    plt.xlim(rmin,rmax)
-    plt.xlabel('r ($\mathrm{\mu m}$)')
-    plt.ylabel('u(r) $(k_B T)$')
-    plt.tight_layout()
-    plt.show()
+fig = plt.figure()
+[plt.plot(plotx,potential[i](plotx),label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
+plt.xlim(rmin,rmax)
+plt.xlabel('r ($\mathrm{\mu m}$)')
+plt.ylabel('u(r) $(k_B T)$')
+plt.tight_layout()
+plt.show()
 
 #plot chi / error as function of iteration number
-with plt.rc_context(params):
-    fig=plt.figure()
-    plt.plot(range(1,len(error)+1),error)
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel('function evaluation')
-    plt.ylabel('χ²')
-    plt.tight_layout()
+fig=plt.figure()
+plt.plot(range(1,len(error)+1),error)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('function evaluation')
+plt.ylabel('χ²')
+plt.tight_layout()
 
 #plot distance histogram and last iteration together
-with plt.rc_context(params):
-    plt.figure()
-    plt.axhline(1,color='k',linewidth=0.75)
-    plt.plot(bincent,binvals,'-r',label='distance histogram')
-    plt.plot(bincent,rdf[-1],'o',color='k',markerfacecolor='none',label='final iteration')
-    plt.xlabel('$r$ ($\mathrm{\mu m}$)')
-    plt.ylabel('$g(r)$')
-    plt.xlim(rmin,rmax)
-    plt.legend(frameon=False)
-    plt.tight_layout()
+plt.figure()
+plt.axhline(1,color='k',linewidth=0.75)
+plt.plot(bincent,binvals,'-r',label='distance histogram')
+plt.plot(bincent,rdf[-1],'o',color='k',markerfacecolor='none',label='final iteration')
+plt.xlabel('$r$ ($\mathrm{\mu m}$)')
+plt.ylabel('$g(r)$')
+plt.xlim(rmin,rmax)
+plt.legend(frameon=False)
+plt.tight_layout()
 
 #plot u(r) of last iteration
-with plt.rc_context(params):
-    plt.figure()
-    plt.axhline(0,color='k',linewidth=0.75)
-    plt.plot(plotx,potential[-1](plotx),'-o',color='k',markeredgecolor='k',markerfacecolor='none')
-    plt.xlabel('$r$ ($\mathrm{\mu m}$)')
-    plt.ylabel('$u(r)$ $(k_B T)$')
-    plt.xlim([rmin,rmax])
-    plt.tight_layout()
+plt.figure()
+plt.axhline(0,color='k',linewidth=0.75)
+plt.plot(plotx,potential[-1](plotx),'-o',color='k',markeredgecolor='k',markerfacecolor='none')
+plt.xlabel('$r$ ($\mathrm{\mu m}$)')
+plt.ylabel('$u(r)$ $(k_B T)$')
+plt.xlim([rmin,rmax])
+plt.tight_layout()
     
 plt.show()
--- examples/run_example_idealgas_3d.py
+++ examples/run_example_idealgas_3d.py
@@ -5,19 +5,6 @@ import numpy as np
 from potentials_from_particle_insertion import run_iteration,rdf_dist_hist_3d
 from potentials_from_particle_insertion.generate_coordinates import _rand_coord_in_box #only for generating fake data
 
-params = {
-    'figure.autolayout' : True,
-    'xtick.top' : False,
-    'xtick.bottom' : True,
-    'xtick.minor.visible' : True,
-    'xtick.labelsize':'medium',
-    'ytick.left' : True,
-    'ytick.right' : False,
-    'ytick.minor.visible' : True,
-    'figure.figsize' : (4, 3),
-    'font.size' : 10
-    }
-
 #%% input parameters
 
 #data generation
@@ -43,28 +30,28 @@ binedges,binvals = rdf_dist_hist_3d(
     rmin=rmin,
     rmax=rmax,
     dr=dr,
+    handle_edge='cuboid',
     boundary=boundary,
-    periodic_boundary=False
 )
 
 bincent = (binedges[1:]+binedges[:-1])/2
 
 #plot rdf
-with plt.rc_context(params):
-    plt.figure()
-    plt.plot(bincent,binvals,'-r',label='dist hist')
-    plt.xlabel('r ($\mathrm{\mu m}$)')
-    plt.ylabel('g(r)')
-    plt.xlim(rmin,rmax)
-    plt.ylim(0,1.1)
-    plt.show()
+plt.figure()
+plt.plot(bincent,binvals,'-r',label='dist hist')
+plt.xlabel('r ($\mathrm{\mu m}$)')
+plt.ylabel('g(r)')
+plt.xlim(rmin,rmax)
+plt.ylim(0,1.1)
+plt.show()
 
 
 #%% run iterations
 error,potential,rdf,counts = run_iteration(
     coords,
     binvals,
-    boundary,
+    handle_edge='cuboid',
+    boundary=boundary,
     rmin=rmin,
     rmax=rmax,
     dr=dr,
@@ -72,7 +59,6 @@ error,potential,rdf,counts = run_iteration(
     convergence_tol=1e-6,
     n_ins=500,
     regulate=True,
-    periodic_boundary=False,
 )
 
 
@@ -82,57 +68,54 @@ cmap = get_cmap('jet')
 colors = [cmap(i/(len(error)-1))[:3]+(0.3,) for i in range(len(error))]
 
 #plot all iterations on top of eachother
-with plt.rc_context(params):
-    fig = plt.figure()
-    plt.plot(bincent,binvals,label='distance histogram',color='k')
-    [plt.plot(bincent,rdf[i],label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
-    plt.xlim(rmin,rmax)
-    plt.xlabel('r ($\mathrm{\mu m}$)')
-    plt.ylabel('g(r)')
-    plt.tight_layout()
-    plt.show()
+fig = plt.figure()
+plt.plot(bincent,binvals,label='distance histogram',color='k')
+[plt.plot(bincent,rdf[i],label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
+plt.xlim(rmin,rmax)
+plt.ylim(0,1.1)
+plt.xlabel('r ($\mathrm{\mu m}$)')
+plt.ylabel('g(r)')
+plt.tight_layout()
+plt.show()
 
 #plot u(r) for all iterations on top of eachother
-with plt.rc_context(params):
-    fig = plt.figure()
-    [plt.plot(bincent,potential[i],label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
-    plt.xlim(rmin,rmax)
-    plt.xlabel('r ($\mathrm{\mu m}$)')
-    plt.ylabel('u(r) $(k_B T)$')
-    plt.tight_layout()
-    plt.show()
+fig = plt.figure()
+[plt.plot(bincent,potential[i],label='particle insertion {:}'.format(i),color=colors[i]) for i in range(len(error))]
+plt.xlim(rmin,rmax)
+plt.xlabel('r ($\mathrm{\mu m}$)')
+plt.ylabel('u(r) $(k_B T)$')
+plt.tight_layout()
+plt.show()
 
 #plot chi / error as function of iteration number
-with plt.rc_context(params):
-    fig=plt.figure()
-    plt.plot(range(1,len(error)+1),error)
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel('iteration step')
-    plt.ylabel('χ²')
-    plt.tight_layout()
-    plt.show()
+fig=plt.figure()
+plt.plot(range(1,len(error)+1),error)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('iteration step')
+plt.ylabel('χ²')
+plt.tight_layout()
+plt.show()
 
 #plot distance histogram and last iteration together
-with plt.rc_context(params):
-    plt.figure()
-    plt.axhline(1,color='k',linewidth=0.75)
-    plt.plot(bincent,binvals,'-r',label='distance histogram')
-    plt.plot(bincent,rdf[-1],'o',color='k',markerfacecolor='none',label='final iteration')
-    plt.xlabel('$r$ ($\mathrm{\mu m}$)')
-    plt.ylabel('$g(r)$')
-    plt.xlim(rmin,rmax)
-    plt.legend(frameon=False)
-    plt.tight_layout()
-    plt.show()
+plt.figure()
+plt.axhline(1,color='k',linewidth=0.75)
+plt.plot(bincent,binvals,'-r',label='distance histogram')
+plt.plot(bincent,rdf[-1],'o',color='k',markerfacecolor='none',label='final iteration')
+plt.xlabel('$r$ ($\mathrm{\mu m}$)')
+plt.ylabel('$g(r)$')
+plt.xlim(rmin,rmax)
+plt.ylim(0,1.1)
+plt.legend()
+plt.tight_layout()
+plt.show()
 
 #plot u(r) of last iteration
-with plt.rc_context(params):
-    plt.figure()
-    plt.axhline(0,color='k',linewidth=0.75)
-    plt.plot(bincent,potential[-1],'-o',color='k',markeredgecolor='k',markerfacecolor='none')
-    plt.xlabel('$r$ ($\mathrm{\mu m}$)')
-    plt.ylabel('$u(r)$ $(k_B T)$')
-    plt.xlim([rmin,rmax])
-    plt.tight_layout()
-    plt.show()
+plt.figure()
+plt.axhline(0,color='k',linewidth=0.75)
+plt.plot(bincent,potential[-1],'-o',color='k',markeredgecolor='k',markerfacecolor='none')
+plt.xlabel('$r$ ($\mathrm{\mu m}$)')
+plt.ylabel('$u(r)$ $(k_B T)$')
+plt.xlim([rmin,rmax])
+plt.tight_layout()
+plt.show()
