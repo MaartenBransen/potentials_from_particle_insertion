715e48a81ead79d9bd72434ef9e839651c269c04
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2024-01-16 12:21:22 +0100

    add overall docstring for the package

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # potentials_from_particle_insertion
 ## Overview
-Python package with a set of functions for calculating the g(r) using the Widom test-particle insertion method, and/or for fitting the g(r) to solve for the pair-potential using particle coordinates from e.g. confocal microscopy
+Python package with a set of functions for calculating the g(r) using the Widom test-particle insertion method, and/or for fitting the g(r) to solve for the pair-potential using particle coordinates from e.g. confocal microscopy or cryo-TEM.
+
+The codes for this package were developed as part of the work in reference [1], where the iterative use of test-particle insertion to solve for the pair potential was based on [2] with correction for periodic or finite boundary conditions based on equations described in [3,4].
+
+### References
+[1] Bransen, M. (2024). Measuring interactions between colloidal (nano)particles. PhD thesis, Utrecht University.
+
+[2] Stones, A. E., Dullens, R. P. A., & Aarts, D. G. A. L. (2019). [Model-Free Measurement of the Pair Potential in Colloidal Fluids Using Optical Microscopy](https://doi.org/10.1103/PhysRevLett.123.098002). Physical Review Letters, 123(9), 098002. 
+
+[3] Markus Seserno (2014). [How to calculate a three-dimensional g(r) under periodic boundary conditions](https://www.cmu.edu/biolphys/deserno/pdf/gr_periodic.pdf).
+
+[4] Kopera, B. A. F., & Retsch, M. (2018). [Computing the 3D Radial DistributionFunction from Particle Positions: An Advanced Analytic Approach](https://doi.org/10.1021/acs.analchem.8b03157). Analytical Chemistry, 90(23), 13909–13914.
 
 ## Installation
 
@@ -33,4 +44,4 @@ from potentials_from_particle_insertion import rdf_dist_hist_3d, run_iteration
 ```
 to only import specific functions.
 
-For a complete API reference see [the documentation](https://maartenbransen.github.io/potentials_from_particle_insertion/) (may be private) or download `index.html` from the `gh-pages` branch and open it with your browser. There are two example usage scripts in `/examples/` for solving for the pair-potential of a randomly generated ideal gas in 2D and 3D in periodic and nonperiodic boundary conditions respectively.
+For a complete API reference see [the documentation](https://maartenbransen.github.io/potentials_from_particle_insertion/) or download `index.html` from the `gh-pages` branch and open it with your browser. There are example usage scripts in `/examples/` for solving for the pair-potential of a randomly generated ideal gas in 2D and 3D in periodic and nonperiodic boundary conditions respectively, as well as examples for multi-component systems and using fit functions.
--- potentials_from_particle_insertion/__init__.py
+++ potentials_from_particle_insertion/__init__.py
@@ -1,6 +1,33 @@
-"""
--------------------------------------------------------------------------------
+"""Python package with a set of functions for calculating the g(r) using the 
+Widom test-particle insertion method, and/or for fitting the g(r) to solve for
+the pair-potential using particle coordinates from e.g. confocal microscopy or 
+cryo-TEM.
 
+The codes for this package were developed as part of the work in reference [1],
+where the iterative use of test-particle insertion to solve for the pair 
+potential was based on [2] with correction for periodic or finite boundary 
+conditions based on equations described in [3,4]
+
+References
+----------
+[1] Bransen, M. (2024). Measuring interactions between colloidal 
+(nano)particles. PhD thesis, Utrecht University.
+
+[2] Stones, A. E., Dullens, R. P. A., & Aarts, D. G. A. L. (2019). Model-Free 
+Measurement of the Pair Potential in Colloidal Fluids Using Optical Microscopy.
+Physical Review Letters, 123(9), 098002. 
+https://doi.org/10.1103/PhysRevLett.123.098002
+
+[3] Markus Seserno (2014). How to calculate a three-dimensional g(r) under
+periodic boundary conditions.
+https://www.cmu.edu/biolphys/deserno/pdf/gr_periodic.pdf
+
+[4] Kopera, B. A. F., & Retsch, M. (2018). Computing the 3D Radial Distribution
+Function from Particle Positions: An Advanced Analytic Approach. Analytical 
+Chemistry, 90(23), 13909–13914.
+https://doi.org/10.1021/acs.analchem.8b03157
+
+-------------------------------------------------------------------------------
 Copyright (c) 2024 Maarten Bransen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,7 +47,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 -------------------------------------------------------------------------------
 """
 
@@ -29,8 +55,6 @@ __version__ = '0.6.0'
 from .pairpotential_iterator import (
     run_iteration,
     run_iterator_fitfunction,
-    #run_iterator_fitfunction2,
-    #run_iterator_fitfunction3,
 )
 
 from .distribution_functions import (
@@ -49,15 +73,13 @@ from .utility import (
 
 #define all for doing `from .. import *`
 __all__ = [
-    'run_iteration',
-    'run_iterator_fitfunction',
-    'run_iterator_fitfunction2',
-    'run_iterator_fitfunction3',
     'rdf_dist_hist_2d',
     'rdf_dist_hist_3d',
     'rdf_insertion_binned_2d',
     'rdf_insertion_binned_3d',
     'rdf_insertion_exact_3d',
+    'run_iteration',
+    'run_iterator_fitfunction',
     'save_TPI_results',
     'load_TPI_results',
 ]
